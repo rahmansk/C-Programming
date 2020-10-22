239d1dfd001776e22196e0c992be997816184d95
Mohamed Rahman Shareff S <62998470+rahmansk@users.noreply.github.com>
2020-10-22 18:36:13 +0530

    Add files via upload

---
+++ 3. Character set.c
@@ -0,0 +1,20 @@
+//Escape Sequence Character
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    printf("Hello\n ");   //New line(line break)
+    printf("Hello\b ");   //Backspace
+    printf("Hello\t ");   //Horizontal Tab
+    printf("Hello\f ");   //Form Feed
+    printf("Hello\a ");   //Alert(alert bell)
+    printf("Hello\r ");   //Carriage Return
+    printf("Hello\v ");   //Vertical Tab
+    printf("Hello\? ");   //Question Mark
+    printf("Hello\' ");   //Single Quote
+    printf("Hello\" ");   //Double Quote
+    printf("Hello\\ ");   //Backslash
+    printf("Hello\0 ");   //Null
+}
