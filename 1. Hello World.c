71ea542e86fc4952e1df6d999e39e0fabc67370c
Mohamed Rahman Shareff S <62998470+rahmansk@users.noreply.github.com>
2020-10-22 14:24:51 +0530

    Add files via upload

---
+++ 1. Hello World.c
@@ -0,0 +1,11 @@
+/*This is the basic C programming Code*/    //Documentation Section
+
+#include <stdio.h>                         //Link Section
+#include <stdlib.h>
+
+
+void main()                                //main() Function Section
+{
+    printf("Hello World!");                //Declaration Part
+    return 0;                              //Executable Part
+}
